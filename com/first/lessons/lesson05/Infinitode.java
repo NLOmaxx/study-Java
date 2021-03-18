5999e1b1d9d61ec75b8b4812a009bec1be84e142
NLOmaxx <77255600+NLOmaxx@users.noreply.github.com>
2021-03-18 19:14:15 +0700

    Add files via upload

---
+++ com/first/lessons/lesson05/Infinitode.java
@@ -0,0 +1,21 @@
+package com.lessons.lesson05;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Infinitode {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        extracted(reader);
+        String str = reader.readLine();
+        while (!str.equals("exit") || !str.equals("EXIT") || !str.equals("Exit")) {
+            
+        }
+
+
+       
+    }
+
+    private static void extracted(BufferedReader reader) throws IOException {
+    }
+}
